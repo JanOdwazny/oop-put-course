c5fbf6323c7988266d8b3954aca4fe857adb6a5a
JanOdwazny <55091932+JanOdwazny@users.noreply.github.com>
2022-10-13 16:39:24 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class dog{
+    public:
+    string breed;
+    string name;
+    int age;
+
+
+};
+
+class car{
+    public:
+    string brand;
+    string model;
+    int year;
+    int isowned;
+};
+class human {
+public:
+    string name;
+    int age;
+    void buy_car(car newcar){
+    newcar.isowned=1
+
+    }
+};
+
+
+int main()
+{
+    car newcar;
+    newcar.brand="opel";
+    newcar.model="astra";
+    human newhuman;
+    newhuman.name="adam";
+    dog dog1;
+    dog1.breed="beagle";
+    dog1.name="jack";
+    dog1.age=5;
+    newhuman.get_car(newcar);
+
+
+    //cout << dog1.breed <<" "<< endl;
+    return 0;
+}
